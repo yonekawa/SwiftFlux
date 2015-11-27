b645e124902ad83b99ac7452f8a3fa60ce9946c2
Kenichi Yonekawa <yonekawa@freee.co.jp>
2015-11-28 03:12:54 +0900

    Experimental Redux implementation

---
+++ SwiftFlux/Utils/Redux.swift
@@ -0,0 +1,57 @@
+//
+//  Redux.swift
+//  SwiftFlux
+//
+//  Created by Kenichi Yonekawa on 11/28/15.
+//  Copyright © 2015 mog2dev. All rights reserved.
+//
+
+public protocol ReduxAction {}
+public typealias ReduxDispatcher = (ReduxAction) -> ()
+public typealias ReduxActionCreator = (dispatch: ReduxDispatcher) -> ()
+
+public class ReduxStore<T: Equatable> {
+    private let initialState: T
+    private let reducer: (T, ReduxAction) -> T
+
+    public typealias Subscriber = () -> ()
+    private var subscribers = [Subscriber]()
+
+    private var internalState: T?
+    public var state: T {
+        return internalState ?? initialState
+    }
+
+    public static func create<T>(initialState: T, reducer: (T, ReduxAction) -> T) -> ReduxStore<T> {
+        return ReduxStore<T>(initialState: initialState, reducer: reducer)
+    }
+
+    deinit {
+        self.subscribers.removeAll()
+    }
+
+    init(initialState: T, reducer: (T, ReduxAction) -> T) {
+        self.initialState = initialState
+        self.reducer = reducer
+    }
+
+    public func dispatch(action: ReduxAction) {
+        let startState = self.state
+        internalState = reducer(self.state, action)
+        if startState != self.state {
+            emitChange()
+        }
+    }
+
+    public func dispatch(actionCreator: ReduxActionCreator) {
+        actionCreator(dispatch: self.dispatch)
+    }
+
+    public func subscribe(subscriber: Subscriber) {
+        subscribers.append(subscriber)
+    }
+
+    public func emitChange() {
+        subscribers.forEach { (subscriber) in subscriber() }
+    }
+}
